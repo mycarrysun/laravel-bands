edaf216581bb8dea5707913b428fcb12fefed2b9
hitittome873 <mike@nextwebllc.com>
2016-12-20 17:52:40 -0500

    Added band_id to validation rules on Album Requests

--- app/Http/Requests/AlbumRequest.php
+++ app/Http/Requests/AlbumRequest.php
@@ -4,33 +4,31 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AlbumRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+class AlbumRequest extends FormRequest {
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize() {
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-	        'name'             => 'required|string|between:0,255',
-	        'recorded_date'    => 'date',
-	        'release_date'     => 'date',
-	        'number_of_tracks' => 'integer',
-	        'label'            => 'string|between:0,255',
-	        'producer'         => 'string|between:0,255',
-	        'genre'            => 'string|between:0,255',
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules() {
+		return [
+			'band_id'          => 'required|exists:bands,id',
+			'name'             => 'required|string|between:0,255',
+			'recorded_date'    => 'date',
+			'release_date'     => 'date',
+			'number_of_tracks' => 'integer',
+			'label'            => 'string|between:0,255',
+			'producer'         => 'string|between:0,255',
+			'genre'            => 'string|between:0,255',
+		];
+	}
 }
